864cb939424f9faaabf344ae4bdc38cb3258e821
johntech-dev <johnayomide50@gmail.com>
2023-09-21 13:38:21 +0100

    j

--- src/Component/DragDrop/Dragdrop.jsx
+++ src/Component/DragDrop/Dragdrop.jsx
@@ -12,15 +12,15 @@ function Dragdrop() {
     fileInputRef.current.click();
   }
 
-//   function openImageModal(image) {
-//     setSelectedImage(image);
-//     setModalOpen(true);
-//   }
+  //   function openImageModal(image) {
+  //     setSelectedImage(image);
+  //     setModalOpen(true);
+  //   }
 
-//   function closeImageModal() {
-//     setSelectedImage(null);
-//     setModalOpen(false);
-//   }
+  //   function closeImageModal() {
+  //     setSelectedImage(null);
+  //     setModalOpen(false);
+  //   }
 
   function onFileSelect(event) {
     const files = event.target.files;
@@ -41,96 +41,96 @@ function Dragdrop() {
   //  function deleteImage(index){
   //     setImages((prevImages) => prevImages.filter((_, i) => i !== index));
   //  }
-   function onDragOver(event) {
-      event.preventDefault();
-      setIsDragging(true);
-      event.dataTransfer.dropEffect = "copy";
-   }
-   function onDragLeave(event) {
+  function onDragOver(event) {
+    event.preventDefault();
+    setIsDragging(true);
+    event.dataTransfer.dropEffect = "copy";
+  }
+  function onDragLeave(event) {
     event.preventDefault();
     setIsDragging(false);
-   }
-   function onDrop(event) {
-      event.preventDefault();
-      setIsDragging(false);
-      const files = event.dataTransfer.files;
-      for (let i = 0; i < files.length; i++) {
-        if (files[i].type.split('/')[0] !== 'image') continue;
-        if (!images.some((e) => e.name === files[i].name)) {
-          setImages((prevImages) => [
-            ...prevImages,
-            {
-              name: files[i].name,
-              url: URL.createObjectURL(files[i]),
-            },
-          ]);
-        }
+  }
+  function onDrop(event) {
+    event.preventDefault();
+    setIsDragging(false);
+    const files = event.dataTransfer.files;
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].type.split('/')[0] !== 'image') continue;
+      if (!images.some((e) => e.name === files[i].name)) {
+        setImages((prevImages) => [
+          ...prevImages,
+          {
+            name: files[i].name,
+            url: URL.createObjectURL(files[i]),
+          },
+        ]);
       }
-    
-   }
-  return ( 
+    }
+
+  }
+  return (
     < div className='bg-black max-h-screen min-h-screen'>
-      
-          <input
+
+      <input
         className='items-center mt-3 flex bg-transparent px-2 py-2 font-semibold text-purple-600 rounded-xl border-none w-[350px] ring-2 ring-gray-300 focus:ring-2 focus:ring-gray-500 mx-auto'
         type='text'
         placeholder='Search by image name'
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
-        />
-       
-     
-    <div className='p-1 text-center pb-6'>
-      <div className='text-center font-bold text-purple-600 text-[30px]'>
-        <p>Drag & Drop Image Uploading</p>
+      />
+
+
+      <div className='p-1 text-center pb-6'>
+        <div className='text-center font-bold text-purple-600 text-[30px]'>
+          <p>Drag & Drop Image Uploading</p>
+        </div>
+        <div className='h-[150px] w-[300px] mx-auto rounded-xl border-[2px] border-dashed border-purple-600 text-purple-500 flex justify-center mt-3  items-center' onDragOver={onDragOver} onDragLeave={onDragLeave} onDrop={onDrop}>
+          {isDragging ? (
+            <span className='justify-center'>Drop Images here</span>
+          ) : (
+            <>
+              {"  Drag & Drop images here or   "} {"         "}
+              <span role='button' onClick={selectFiles}>
+                Browse
+              </span>
+            </>
+          )}
+          <input
+            name='file'
+            type='file'
+            multiple
+            ref={fileInputRef}
+            onChange={onFileSelect}
+            className='display'
+          />
+        </div>
+        <button
+          type='button'
+          className='border p-[10px] cursor-pointer bg-purple-400 text-white mt-[20px] w-[40%]'
+        >
+          Upload
+        </button>
+
+
       </div>
-      <div className='h-[150px] w-[300px] mx-auto rounded-xl border-[2px] border-dashed border-purple-600 text-purple-500 flex justify-center mt-3  items-center' onDragOver={onDragOver} onDragLeave={onDragLeave} onDrop={onDrop}>
-        {isDragging ? (
-          <span className='justify-center'>Drop Images here</span>
-        ) : (
-          <>
-            {"  Drag & Drop images here or   "} {"         "}
-            <span role='button' onClick={selectFiles}>
-               Browse
-            </span>
-          </>
-        )}
-        <input
-          name='file'
-          type='file'
-          multiple
-          ref={fileInputRef}
-          onChange={onFileSelect}
-          className='display'
-        />
-      </div>
-      <button
-        type='button' 
-        className='border p-[10px] cursor-pointer bg-purple-400 text-white mt-[20px] w-[40%]'
-      >
-        Upload
-      </button>
-      
-    
-    </div>
-    
+
       <section className='grid grid-cols-2 ml-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 max-h-24 max-w-24 min-h-0 justify-between items-center gap-6'>
-         
-         {images
+
+        {images
           .filter((image) =>
             image.name.toLowerCase().includes(searchQuery.toLowerCase())
           )
-        .map((image, index) => (
-          <div className=' w-28 flex flex-col rounded-lg ' key={index}>
-          <img className='w-28 rounded-xl'
-          src={image.url} alt={image.name} /> 
-          <p className='text-purple-600'>{image.name} </p>
-          </div>
+          .map((image, index) => (
+            <div className=' w-28 flex flex-col rounded-lg ' key={index}>
+              <img className='w-28 rounded-xl'
+                src={image.url} alt={image.name} />
+              <p className='text-purple-600'>{image.name} </p>
+            </div>
           ))}
-        
-         
+
+
       </section>
-      </div>
+    </div>
   );
 }
 
--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../Service/firebaseConfig'
+import { auth } from '../../Service/firebaseConfig';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -10,7 +10,7 @@ function Login() {
   const handleLogin = (e) => {
     e.preventDefault();
 
-  
+
     // Check if the provided email and password match the specific user's credentials
     if (email === 'user@example.com' && password === '1Password') {
       signInWithEmailAndPassword(auth, email, password)
@@ -29,7 +29,7 @@ function Login() {
   };
   const navigate = useNavigate();
 
-  function handleClick () {
+  function handleClick() {
     if (email === 'user@example.com' && password === '1Password') {
       signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
@@ -47,29 +47,29 @@ function Login() {
   };
   return (
     <div className='flex justify-center items-center h-screen bg-purple-700' >
-    <div className='w-96 p-6 shadow-lg bg-white rounded-md'>
-      <form onSubmit={handleLogin}>
-        <h1 className='text-center text-lg'>Login</h1>
-        <label className='block text-base mb-2'>Email Address</label>
-        <input
-          type='email'
-          placeholder='Email address'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-purple-600 rounded-lg'/>
-        <label className='block text-base mt-8'>Password</label>
-        <input
-          type='password'
-          placeholder='Enter password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-purple-600 rounded-lg mt-5'
-        />
-        <button type='submit' onClick={handleClick} onSubmit={handleLogin} className='border w-50% text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-purple-600 rounded-lg mt-5 justify-center items-center ml-[40%]' >Login</button>
-      </form>
-    </div>
+      <div className='w-96 p-6 shadow-lg bg-white rounded-md'>
+        <form onSubmit={handleLogin}>
+          <h1 className='text-center text-lg'>Login</h1>
+          <label className='block text-base mb-2'>Email Address</label>
+          <input
+            type='email'
+            placeholder='Email address'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-purple-600 rounded-lg' />
+          <label className='block text-base mt-8'>Password</label>
+          <input
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-purple-600 rounded-lg mt-5'
+          />
+          <button type='submit' onClick={handleClick} onSubmit={handleLogin} className='border w-50% text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-purple-600 rounded-lg mt-5 justify-center items-center ml-[40%]' >Login</button>
+        </form>
+      </div>
     </div>
   )
 }
